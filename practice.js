1e2af8fefea24d09ac98a5cc756957075bba54fe
Joshua Sandberg <jshsandberg@gmail.com>
2020-06-29 23:18:49 -0700

    final turn in; however, want to go back and try to fix it

--- practice.js
+++ practice.js
@@ -7,10 +7,13 @@ let numbersEl = document.getElementById(`numbers`);
 let symbolsEl = document.getElementById(`symbols`);
 let generateEl = document.getElementById(`generate`);
 
+//Have no idea how to implement the length of the password into the program I have built. Was thinking of making the button run the function multiple times until it hit the target length; however, could not figure it out with the syntax but I think my psuedocode was correct
+//console.log(lengthEl)
 
-console.log(lengthEl)
+//trying to figure out how to add to the array instead of replacing the randomly chosen numbers
+let segment = [``];
 
-
+//Was trying to creat an object that would make running the function smoother but could not figure this out
 let randomFunc = {
     lower: getRandomLower,
     upper: getRandomUpper,
@@ -18,6 +21,8 @@ let randomFunc = {
     symbol: getRandomSymbol,
 };
 
+
+//This was going to allow me to check to see if boxes were checked to be true with the event on line 38
 generateEl.addEventListener(`click`, () => {
     let lengthPassword = parseInt(lengthEl.value);
     let hasLower = lowerEl.checked;
@@ -25,26 +30,41 @@ generateEl.addEventListener(`click`, () => {
     let hasNumber = numbersEl.checked; 
     let hasSymbol = symbolsEl.checked;  
 
-    console.log(hasLower, hasUpper, hasNumber, hasSymbol);
+    console.log(lengthPassword, hasLower, hasUpper, hasNumber, hasSymbol);
 });
-    
+
+
+    //Trying to figure out how I can create if statements when checking to see if boxes are checked
 generateEl.addEventListener(`click`, () => {
-  a = getRandomLower();
+  if (hasLower = true) {
+      a = getRandomLower();
+    } else ( return );
   b = getRandomUpper();
   c = getRandomNumber();
   d = getRandomSymbol();  
-  console.log(a, b, c, d);
+  segment.push = [a, b, c, d];
+  console.log(segment)
 }) 
+//I am getting the correct array of numbers; however, I do not know how to put it on the website without using console.log
+console.log(segment)
 
+//was trying to create a function so when I would run line 38, I could just put the function in instead of putting the whole thing in, could not figure it out with the syntax, kept messing up with the parentheses and curly brackets
+//I feel like if I could figure how to implement this function into the event on line 38, it would allow me to create multiple arrays much easier
+function getPassword() {
+    a = getRandomLower();
+    b = getRandomUpper();
+    c = getRandomNumber();
+    d = getRandomSymbol();
 
+} 
  
-
+//creating the functions to get the numbers, symbols, and letters were pretty easy. I tried doing an array but remebered bryan brought up the charcode so checked that out and made this step much easier
 function getRandomLower(a) {
     return String.fromCharCode(Math.floor(Math.random() * 26 ) + 97);
 }
     
 
-console.log(getRandomLower());
+//console.log(getRandomLower());
 
 
 function getRandomUpper(b) {
